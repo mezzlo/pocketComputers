446c246e3e1dd5acd53b33533a8344d299a89ff0
mezzlo <71941530+mezzlo@users.noreply.github.com>
2026-03-14 03:07:35 -0400

    Add files via upload

---
+++ testingStartui.lua
@@ -0,0 +1,39 @@
+-- Basalt 2 Generated Code
+local basalt = require("basalt")
+
+-- Create main frame
+local main = basalt.createFrame()
+    :setSize(26, 20)
+
+-- Flexbox element
+local element1 = main:addFlexbox()
+    :setSize(26, 20)
+
+-- Label element
+local element2 = main:addLabel()
+    :setPosition(6, 3)
+    :setSize(17, 1)
+    :setText("What's your name?")
+
+-- Label element
+local element3 = main:addLabel()
+    :setPosition(6, 4)
+    :setSize(6, 1)
+    :setText("Enter:")
+
+-- Input element
+local element4 = main:addInput()
+    :setPosition(12, 4)
+    :setSize(11, 1)
+    :setPlaceholder("BurgerBob")
+    :setBackground(colors.lightBlue)
+
+-- Button element
+local element5 = main:addButton()
+    :setPosition(8, 14)
+    :setSize(12, 3)
+    :setText("Confirm")
+    :setBackground(colors.green)
+
+-- Start the UI
+basalt.autoUpdate()
