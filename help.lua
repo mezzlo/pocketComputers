8f707a136051680e5d78e268b79f962d43ee8e30
mezzlo <71941530+mezzlo@users.noreply.github.com>
2026-03-14 15:58:14 -0400

    Update help.lua

--- help.lua
+++ help.lua
@@ -1,1 +1,10 @@
 -- help to run basalt download
+
+-- download startUp
+shell.run("wget https://github.com/mezzlo/pocketComputers/raw/refs/heads/main/startup.lua")
+
+-- Download main
+shell.run("wget https://github.com/mezzlo/pocketComputers/raw/refs/heads/main/main.lua")
+
+--download init
+shell.run("wget https://github.com/mezzlo/pocketComputers/raw/refs/heads/main/init.lua")
